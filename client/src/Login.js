9397b167b4f866c69b956ecdefdf7023035032bd
Klaudia Bonter <klaudiabonter@gmail.com>
2025-04-09 14:36:49 +0200

    change background

--- client/src/Login.js
+++ client/src/Login.js
@@ -46,7 +46,7 @@ function Login() {
 
   return (
         <div style={{
-      backgroundImage: 'url("/images/background.jfif")',
+      backgroundImage: 'url("/images/background.png")',
       backgroundSize: 'cover',       // skalowanie do wielkości kontenera
       backgroundRepeat: 'no-repeat', // wyłączenie powtarzania
       backgroundPosition: 'center',  // wyśrodkowanie obrazu     
