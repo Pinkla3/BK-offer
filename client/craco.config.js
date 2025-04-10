1f5439ea6ab88f74aea2e45827fbcdf8d1401ba3
Klaudia Bonter <klaudiabonter@gmail.com>
2025-04-10 22:30:00 +0200

    fix

--- client/craco.config.js
+++ client/craco.config.js
@@ -1,31 +1,8 @@
 module.exports = {
   devServer: {
-    host: '0.0.0.0',               // 👈 to dodaj
-    allowedHosts: 'all',
+    host: '0.0.0.0',
     port: 3008,
-    historyApiFallback: true,
-    proxy: {
-      '/login': 'http://localhost:3009',
-      '/register': 'http://localhost:3009',
-      '/check-email': 'http://localhost:3009',
-      '/change-password': 'http://localhost:3009',
-      '/reset-password': 'http://localhost:3009',
-      '/entries': 'http://localhost:3009',
-      '/api': 'http://localhost:3009'
-    }
-  },
-
-  webpack: {
-    configure: (webpackConfig, { env, paths }) => {
-      if (env === 'development') {
-        webpackConfig.devServer = {
-          ...webpackConfig.devServer,
-          client: {
-            webSocketURL: 'wss://bk-offer.pl/ws'
-          }
-        };
-      }
-      return webpackConfig;
-    }
+    allowedHosts: 'all',
+    historyApiFallback: true
   }
 };
