c272db0836c4b372cae17a44c0b76efd643e75dc
Klaudia Bonter <klaudiabonter@gmail.com>
2025-04-14 18:44:02 +0200

    add pagination and jusify

--- client/src/TabViewData.js
+++ client/src/TabViewData.js
@@ -332,7 +332,7 @@ function TabViewData({ user }) {
                     textAlign: 'left',
                     borderBottom: '1px solid #e0e0e0',
                     cursor: 'pointer',
-                    minWidth: '100px'
+                    minWidth: '20px'
                   }}
                   onClick={() => handleSort(col)}
                 >
