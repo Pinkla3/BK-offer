1330134925174ef86179dee28fb8133899d821b0
Klaudia Bonter <klaudiabonter@gmail.com>
2025-05-12 00:48:33 +0200

    delete domain

--- client/src/Login.js
+++ client/src/Login.js
@@ -4,7 +4,7 @@ import { toast } from 'react-toastify';
 import { useNavigate } from 'react-router-dom';
 import { setToken } from './auth';
 
-const API_BASE_URL = process.env.REACT_APP_API_URL;
+const API_BASE_URL = 'https://desk.berlin-opiekunki.pl';
 
 function decodeToken(token) {
   try {
